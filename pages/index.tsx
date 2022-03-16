8decaef29b40e3c9da158b4da0c46c104dade974
Sergey L <dzirtdo2014@mail.ru>
2022-03-16 08:47:17 +0300

    start commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "./header/header";
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+      <React.Fragment>
+        <Header/>
+        <main>
+          Content
+        </main>
+        <footer>
+          <h3>footer</h3>
+        </footer>
+      </React.Fragment>
+  )
+}
