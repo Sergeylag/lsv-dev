c6368fb2b4ca6a9479f3aa9611e8d856c838c0c9
Sergeylag <dzirtdo2014@mail.ru>
2022-03-17 15:39:15 +0300

    add about

--- pages/header/nav_bar/nav_toggle.tsx
+++ pages/header/nav_bar/nav_toggle.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import About from "./about";
 
 const Toggle = styled.ul`
   align-items: center;
@@ -72,38 +73,12 @@ const Toggle = styled.ul`
 export default function NavToggle() {
     return (
         <Toggle>
-
             <li>
                 <a href="/">Главная</a>
             </li>
             <li>
                 <a href="/about">О больнице</a>
-                <ul>
-                    <li>
-                        <a href="/about/history">История стационара</a>
-                    </li>
-                    <li>
-                        <a href="/about/s-i-spasokukotskij">О Сергее Ивановиче Спасокукоцком</a>
-                    </li>
-                    <li>
-                        <a href="#">Сведения о ГКБ им. С.И. Спасокукоцкого</a>
-                    </li>
-                    <li>
-                        <a href="/about/documents">Официальные документы</a>
-                    </li>
-                    <li>
-                        <a href="/about/administration">Руководство больницы</a>
-                    </li>
-                    <li>
-                        <a href="/about/moscow-doctor">Московский врач</a>
-                    </li>
-                    <li>
-                        <a href="/about/university">Кафедры и университетские клиники</a>
-                    </li>
-                    <li>
-                        <a href="/about/vacancies">Вакансии</a>
-                    </li>
-                </ul>
+                <About/>
             </li>
             <li>
                 <a href="/patients">Пациентам</a>
